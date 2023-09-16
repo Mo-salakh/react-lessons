c608774740888a765c71c61ccb897b6d607e84b0
MacBook <MacBook@MacBook-Air.local>
2023-09-16 23:40:47 +0400

    28-lessonReact

---
+++ src/components/Alert/AlertDefault.jsx
@@ -0,0 +1,16 @@
+
+
+const AlertDefault = (props) => {
+
+    return(
+        <div>
+            <p>{props.nameAlert}</p>
+            <p>{props.ageAlert}</p>
+            <p>{props.cityAlert}</p>
+            <p>{props.familyAlert}</p>
+        </div>
+    )
+
+}
+
+export default AlertDefault;
