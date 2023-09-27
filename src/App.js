78678b44384b3d28a639cd2cf066961e04c08ab1
MacBook <MacBook@MacBook-Air.local>
2023-09-27 18:27:21 +0400

    todo list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,75 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './App.css'
-import Messages from './components/Messages';
+import { useState } from 'react';
 
 
 
 function App() {
 
+
+
+  const [todos, setTodos] = useState([
+    {
+      text: 'step1'
+    },
+    {
+      text: 'step2'
+    },
+    {
+      text: 'step3'
+    }
+  ])
+
+  const [text, setText] = useState('')
+
+
+
+  function handleChange(e){
+    setText(e.target.value)
+
+  }
+
+  function handleBtn() {
+    setTodos([
+      ...todos,
+      {
+        text: text
+      },
+  
+    ])
+  }
+
+  console.log(text);
   return (
-    <div className='container'>
-      <Messages />
+
+    
+    <div className='container p-4'>
+
+
+      <input placeholder='log' value={text} onChange={handleChange} />     {/*а еще можно просто написать название функции в событии onChange={handleChange}*/} 
+      <button onClick={handleBtn}>Log in</button> 
+      
+      {
+      todos.map((item, index) => {
+        return(
+          <div key={index}>
+           {item.text}
+          </div>
+        )
+      })
+      }   
+   
     </div>
+
+
+
+
     
 
+
   )
+
 
 
 
